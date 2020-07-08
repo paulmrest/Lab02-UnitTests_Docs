02045fcb491f159b5ed7553e78fdf8efbe36e3f5
paulmrest <paulmrest@gmail.com>
2020-07-07 19:14:26 -0700

    Testing code in place.

--- Lab02-UnitTest_Docs/Program.cs
+++ Lab02-UnitTest_Docs/Program.cs
@@ -5,13 +5,16 @@ namespace Lab02_UnitTest_Docs
     public class BobsBodaciousBank
     {
 
-        public static decimal Balance;
+        public static decimal Balance = 0.0m;
 
         static void Main(string[] args)
         {
             ATMInterface();
         }
 
+        /// <summary>
+        /// Main interface for ATM. Allows the user to view their balance, make a deposit, a withdrawal, or exit.
+        /// </summary>
         public static void ATMInterface()
         {
             Console.WriteLine("Welcome to Bob's Bodacious Bank!");
@@ -34,8 +37,10 @@ namespace Lab02_UnitTest_Docs
                     switch (userOption)
                     {
                         case 1:
+                            Console.WriteLine("Your current balance is: ${0}", ViewBalance());
                             break;
                         case 2:
+                            Withdrawal(0.00m);
                             break;
                         case 3:
                             break;
@@ -52,7 +57,57 @@ namespace Lab02_UnitTest_Docs
                     Console.WriteLine("Please enter your choice as a number between 1 and 4.");
                     Console.WriteLine(e.Message);
                 }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("An OverflowException occurred.");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                }
+                finally
+                {
+                    Console.WriteLine("Transaction complete. Please choose another or exit.");
+                }
             }
+        }
+
+        /// <summary>
+        /// Returns the current balance.
+        /// </summary>
+        /// <returns>Decimal</returns>
+        public static decimal ViewBalance()
+        {
+            return Balance;
+        }
+
+        /// <summary>
+        /// Removes the specified amount from the account. Will not allow account to drop below $0.00. 
+        /// Only accept positive numbers. Returns the balance after the withdrawal.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns>Decimal</returns>
+        public static decimal Withdrawal(decimal amount)
+        {
+            if (amount < 0.00m)
+            {
+                throw new InvalidOperationException("Withdrawal amount must be ");
+            }
+            return ViewBalance();
+        }
+
+        /// <summary>
+        /// Deposits the specified amount into the account. Only accepts positive numbers.
+        /// Returns the balance after the deposit.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public static decimal Deposit(decimal amount)
+        {
+            return ViewBalance();
         }
     }
 }
--- Lab02Test/BobsBodaciousBankTest.cs
+++ Lab02Test/BobsBodaciousBankTest.cs
@@ -7,6 +7,63 @@ namespace Lab02Test
     public class BobsBodaciousBankTest
     {
         [Fact]
+        public void BalanceReturnsADecmialValueGreaterThanZero()
+        {
+            Assert.InRange(ViewBalance(), 0.0m, Decimal.MaxValue);
+        }
 
+        [Fact]
+        public void WithdrawalInvalidInput()
+        {
+            Assert.Throws<InvalidOperationException>(() => Withdrawal(-5.00m));
+        }
+
+        [Fact]
+        public void DepositInvalidInput()
+        {
+            Assert.Throws<InvalidOperationException>(() => Deposit(-5.00m));
+        }
+
+        [Fact]
+        public void BalanceAfterDeposit()
+        {
+            decimal amount = 500.00m;
+            Deposit(amount);
+            Assert.Equal(amount, ViewBalance());
+        }
+
+        [Fact]
+        public void BalanceAfterDespositAndWithdrawl()
+        {
+            decimal amount = 1500.00m;
+            decimal withdrawalAmount = 750.00m;
+            Deposit(amount);
+            Withdrawal(withdrawalAmount);
+            Assert.Equal(amount - withdrawalAmount, ViewBalance());
+        }
+
+        [Fact]
+        public void BalanceUnchangedAfterNegativeWithdrawal()
+        {
+            Balance = 500.00m;
+            decimal balance = ViewBalance();
+            Assert.Equal(balance, Withdrawal(-250.00m));
+        }
+
+        [Fact]
+        public void BalanceUnchangedAFterNegativeDeposit()
+        {
+            Balance = 500.00m;
+            decimal balance = ViewBalance();
+            Assert.Equal(balance, Deposit(-250.00m));
+        }
+
+        [Fact]
+        public void BalanceCannotGoNegative()
+        {
+            Balance = 500.00m;
+            decimal balance = ViewBalance();
+            Assert.Equal(balance, Withdrawal(750.00m));
+        }
     }
 }
